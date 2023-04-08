cb7a75e46da71e9aa1fa3afc304c13261c3616b3
Yahor Tsytsarkin <egormail88@gmail.com>
2023-04-08 13:49:37 +0300

    feat: realize burger menu at Pets pag

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -10,15 +10,51 @@ console.log(
 console.log(
   "[+] Верстка обеих страниц валидная: для проверки валидности вёрстки используйте сервис https://validator.w3.org/ (8)"
 );
+// Бургер начало
 
-document
-  .querySelector(".burger_wrap")
-  .addEventListener("click", () =>
-    document.querySelector(".wrapper").classList.toggle("open")
-  );
+const burger = document.querySelector(".burger_wrap");
+const headerLink = document.querySelectorAll(".header_link");
+const burgerMenu = document.querySelector(".header_nav");
 
-document
-  .querySelector(".burger_wrap")
-  .addEventListener("click", () =>
-    document.querySelector("body").classList.toggle("noScroll")
-  );
+burger.addEventListener("click", () =>
+  document.querySelector(".wrapper").classList.toggle("open")
+);
+burger.addEventListener("click", () =>
+  document.querySelector(".blackout").classList.toggle("on")
+);
+
+burger.addEventListener("click", () =>
+  document.querySelector("body").classList.toggle("noScroll")
+);
+document.addEventListener("click", (event) => {
+  if (!burger.contains(event.target) && !burgerMenu.contains(event.target)) {
+    document.querySelector(".wrapper").classList.remove("open");
+  }
+});
+document.addEventListener("click", (event) => {
+  if (!burger.contains(event.target) && !burgerMenu.contains(event.target)) {
+    document.querySelector(".blackout").classList.remove("on");
+  }
+});
+document.addEventListener("click", (event) => {
+  if (!burger.contains(event.target) && !burgerMenu.contains(event.target)) {
+    document.querySelector("body").classList.remove("noScroll");
+  }
+});
+
+headerLink.forEach((el) =>
+  el.addEventListener("click", () =>
+    document.querySelector(".wrapper").classList.remove("open")
+  )
+);
+headerLink.forEach((el) =>
+  el.addEventListener("click", () =>
+    document.querySelector(".blackout").classList.remove("on")
+  )
+);
+
+headerLink.forEach((el) =>
+  el.addEventListener("click", () =>
+    document.querySelector("body").classList.remove("noScroll")
+  )
+);
