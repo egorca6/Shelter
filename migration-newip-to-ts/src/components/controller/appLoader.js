a3620a70e527f93c5de9e39ffddd510ac4957300
Yahor Tsytsarkin <egormail88@gmail.com>
2023-06-10 12:18:30 +0300

    feat: add apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '84f7a50a0e6d4ead8966edb04995cb58', // получите свой ключ https://newsapi.org/
         });
     }
 }
