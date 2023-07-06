f6ac0edbddd87d4a8f6b4098165795cbc39db060
Yahor Tsytsarkin <egormail88@gmail.com>
2023-07-06 11:39:18 +0300

    refactor: constants.ts

--- RSS-CSS-Selectors/src/constants.ts
+++ RSS-CSS-Selectors/src/constants.ts
@@ -1,162 +1,114 @@
 import hljs from 'highlight.js';
 import 'highlight.js/styles/a11y-dark.css';
 
+function createEl(elementName: string, className?: string, content?: string): HTMLElement {
+    const element = document.createElement(elementName);
+    if (className) {
+        element.classList.add(className);
+    }
+    if (content) {
+        element.textContent = content;
+    }
+
+    return element;
+}
+
 export function buildHtml() {
-    const container = document.createElement('main');
-    container.classList.add('container');
+    const container = createEl('main', 'container');
+    const section = createEl('section', 'section');
+    const heading = createEl('h2', 'h2', 'Select the coconut and apple');
+    const help = createEl('button', 'help', `Help, I'm stack`);
+    const tableContainer = createEl('div', 'table-container');
+    const codeContainer = createEl('div');
+    const preElement = createEl('pre');
+    const codeElement = createEl('code');
+    codeElement.classList.add('hljs', 'language-xml');
+    codeElement.textContent = `
+    <div class="table">
 
-    const section = document.createElement('section');
-    section.classList.add('section');
+        <plate>
+            <coconut />
+        <plate />
+        <apple />
+        <plate>
+            <potato />
+        <plate />
+        <plate />
+        <coconut />
 
-    const heading = document.createElement('h2');
-    heading.classList.add('h2');
-    heading.textContent = 'Select the coconut and apple';
+    </div>
+    `;
 
-    const help = document.createElement('button');
-    help.classList.add('help');
-    help.textContent = `Help, I'm stack`;
-    const tableContainer = document.createElement('div');
-    tableContainer.classList.add('table-container');
+    const plate1 = createEl('plate');
+    const plate2 = createEl('plate');
+    const plate3 = createEl('plate');
+    const coconut1 = createEl('coconut');
+    const coconut2 = createEl('coconut');
+    const apple1 = createEl('apple');
+    const potato1 = createEl('potato');
 
-    const codeContainer = document.createElement('div');
-    const preElement = document.createElement('pre');
-    const codeElement = document.createElement('code');
+    const inputContainer = createEl('div', 'input-container');
+    const inputWrapper = createEl('div', 'input-wrapper');
+    const inputHeaderViewer = createEl('div', 'input-wrapper-header', 'HTML  Viewer');
+    const viewerHeaderName = createEl('div', 'input-wrapper-name', 'table.html');
+    const inputHeader = createEl('div', 'input-wrapper-header', 'CSS editor');
+    const inputHeaderName = createEl('div', 'input-wrapper-name', 'style.css');
+    const viewerMain = createEl('div', 'viewer-wrapper-main');
+    const inputHeaderMain = createEl('div', 'input-wrapper-main');
+    const lineNumbers = createEl('div', 'lineNumbers');
 
-    const plate1 = document.createElement('plate');
-    const plate2 = document.createElement('plate');
-    const plate3 = document.createElement('plate');
-    const coconut1 = document.createElement('coconut');
-    const coconut2 = document.createElement('coconut');
-    const apple1 = document.createElement('apple');
-    const potato1 = document.createElement('potato');
-
-    const inputContainer = document.createElement('div');
-    inputContainer.classList.add('input-container');
-
-    const inputWrapper = document.createElement('div');
-    inputWrapper.classList.add('input-wrapper');
-
-    const inputHeaderViewer = document.createElement('div');
-    inputHeaderViewer.classList.add('input-wrapper-header');
-    inputHeaderViewer.textContent = `HTML  Viewer`;
-
-    const viewerHeaderName = document.createElement('div');
-    viewerHeaderName.classList.add('input-wrapper-name');
-    viewerHeaderName.textContent = `table.html`;
-
-    const inputHeader = document.createElement('div');
-    inputHeader.classList.add('input-wrapper-header');
-    inputHeader.textContent = `CSS editor`;
-
-    const inputHeaderName = document.createElement('div');
-    inputHeaderName.classList.add('input-wrapper-name');
-    inputHeaderName.textContent = `style.css`;
-
-    const viewerMain = document.createElement('div');
-    viewerMain.classList.add('viewer-wrapper-main');
-
-    const highlight = document.createElement('div');
-    highlight.classList.add('highlight');
-
-    const inputHeaderMain = document.createElement('div');
-    inputHeaderMain.classList.add('input-wrapper-main');
-
-    const lineNumbers = document.createElement('div');
-    lineNumbers.classList.add('lineNumbers');
     lineNumbers.innerHTML = ``;
     for (let i = 1; i <= 20; i += 1) {
-        const lineNumber = document.createElement('div');
+        const lineNumber = createEl('div');
         lineNumber.textContent = i.toString();
         lineNumbers.append(lineNumber);
     }
 
-    const lineNumbersForViewer = document.createElement('div');
-    lineNumbersForViewer.classList.add('lineNumbers-viewer');
+    const lineNumbersForViewer = createEl('div', 'lineNumbers-viewer');
+
     lineNumbersForViewer.innerHTML = ``;
     for (let i = 1; i <= 20; i += 1) {
-        const lineNumber = document.createElement('div');
+        const lineNumber = createEl('div');
         lineNumber.textContent = i.toString();
         lineNumbersForViewer.append(lineNumber);
     }
 
-    const viewerWrapper = document.createElement('div');
-    viewerWrapper.classList.add('viewer-wrapper');
+    const viewerWrapper = createEl('div', 'viewer-wrapper');
 
-    const input = document.createElement('input');
+    const input = createEl('input');
     input.setAttribute('placeholder', 'Type in a CSS selector');
     input.autofocus = true;
 
-    const inputText = document.createElement('div');
-    inputText.classList.add('input-text');
+    const inputText = createEl('div', 'input-text');
     inputText.innerHTML = `{ <br> /* Styles would go here. */ <br>} <br> <br> /* <br>Type a number to skip to a level. 
     <br>Ex -> "5" for level 5 <br> */`;
 
-    const enter = document.createElement('button');
-    enter.classList.add('enter');
-    enter.textContent = `Enter`;
-
-    const levelContainer = document.createElement('aside');
-    levelContainer.classList.add('level-container');
-
-    const navbar = document.createElement('div');
-    navbar.classList.add('navbar');
-    const buttonWrapper = document.createElement('div');
-    buttonWrapper.classList.add('buttonWrapper');
-
-    const lvlInfo = document.createElement('div');
-    lvlInfo.classList.add('lvlInfo');
-    lvlInfo.textContent = `Level 1 of 10`;
-
-    const prevLvl = document.createElement('button');
-    prevLvl.textContent = `<`;
-    prevLvl.classList.add('prevLvl');
-
-    const nextLvl = document.createElement('button');
-    nextLvl.textContent = `>`;
-    nextLvl.classList.add('nextLvl');
-
-    const buttonMenu = document.createElement('button');
+    const enter = createEl('button', 'enter', 'Enter');
+    const levelContainer = createEl('aside', 'level-container');
+    const navbar = createEl('div', 'navbar');
+    const buttonWrapper = createEl('div', 'buttonWrapper');
+    const lvlInfo = createEl('div', 'lvlInfo', 'Level 1 of 10');
+    const prevLvl = createEl('button', 'prevLvl', '<');
+    const nextLvl = createEl('button', 'nextLvl', '>');
+    const buttonMenu = createEl('button', 'navbar-menu');
     buttonMenu.innerHTML = `&#8801;`;
-    buttonMenu.classList.add('navbar-menu');
 
-    const listLvl = document.createElement('div');
-    listLvl.classList.add('close');
-    const lvl1 = document.createElement('div');
-    lvl1.classList.add('div-lvl');
-    lvl1.textContent = '1  A, B';
-    const lvl2 = document.createElement('div');
-    lvl2.classList.add('div-lvl');
-    lvl2.textContent = '2  A B';
-    const lvl3 = document.createElement('div');
-    lvl3.classList.add('div-lvl');
-    lvl3.textContent = '3  A + B';
-    const lvl4 = document.createElement('div');
-    lvl4.classList.add('div-lvl');
-    lvl4.textContent = '4  You can do it...';
-    const lvl5 = document.createElement('div');
-    lvl5.classList.add('div-lvl');
-    lvl5.textContent = '5  You can do it...';
-    const lvl6 = document.createElement('div');
-    lvl6.classList.add('div-lvl');
-    lvl6.textContent = '6  :not(X)';
-    const lvl7 = document.createElement('div');
-    lvl7.textContent = '7  :first-of-type';
-    lvl7.classList.add('div-lvl');
-    const lvl8 = document.createElement('div');
-    lvl8.classList.add('div-lvl');
-    lvl8.textContent = '8  :empty';
-    const lvl9 = document.createElement('div');
-    lvl9.textContent = '9  :first-child';
-    lvl9.classList.add('div-lvl');
-    const lvl10 = document.createElement('div');
-    lvl10.classList.add('div-lvl');
-    lvl10.textContent = '10  :last-of-type';
-    const reset = document.createElement('button');
-    reset.classList.add('reset');
-    reset.textContent = 'Reset';
+    const listLvl = createEl('div', 'close');
+    const lvl1 = createEl('div', 'div-lvl', '1  A, B');
+    const lvl2 = createEl('div', 'div-lvl', '2  A B');
+    const lvl3 = createEl('div', 'div-lvl', '3  A + B');
+    const lvl4 = createEl('div', 'div-lvl', '4  You can do it...');
+    const lvl5 = createEl('div', 'div-lvl', '5  You can do it...');
+    const lvl6 = createEl('div', 'div-lvl', '6  :not(X)');
+    const lvl7 = createEl('div', 'div-lvl', '7  :first-of-type');
+    const lvl8 = createEl('div', 'div-lvl', '8  :empty');
+    const lvl9 = createEl('div', 'div-lvl', '9  :first-child');
+    const lvl10 = createEl('div', 'div-lvl', '10  :last-of-type');
+    const reset = createEl('button', 'reset', 'Reset');
+
     listLvl.append(lvl1, lvl2, lvl3, lvl4, lvl5, lvl6, lvl7, lvl8, lvl9, lvl10, reset);
 
-    const lvlDiscription = document.createElement('div');
+    const lvlDiscription = createEl('div', 'lvl-discription');
     lvlDiscription.innerHTML = `Comma Combinator<br>
         Combine, selectors, with... commas!<br><br>
       A, B<br><br>
@@ -166,7 +118,6 @@ export function buildHtml() {
       Examples<br>
       div, .fun selects all &lt;div&gt;  elements as well as<br>
       all elements with class="fun"`;
-    lvlDiscription.classList.add('lvl-discription');
 
     section.append(heading);
     section.append(help);
@@ -194,23 +145,6 @@ export function buildHtml() {
     viewerWrapper.append(viewerMain);
     viewerMain.append(lineNumbersForViewer);
 
-    codeElement.classList.add('hljs', 'language-xml');
-    codeElement.textContent = `
-    <div class="table">
-
-        <plate>
-            <coconut />
-        <plate />
-        <apple />
-        <plate>
-            <potato />
-        <plate />
-        <plate />
-        <coconut />
-
-    </div>
-    `;
-
     preElement.append(codeElement);
     codeContainer.append(preElement);
 
@@ -229,13 +163,13 @@ export function buildHtml() {
 
     document.body.append(container);
 
-    const copyrightElement = document.createElement('div');
+    const copyrightElement = createEl('div');
     copyrightElement.innerHTML = '&copy; 2023';
-    const footerWrapper = document.createElement('div');
-    footerWrapper.classList.add('footerWrapper');
+    const footerWrapper = createEl('div', 'footerWrapper');
     section.append(footerWrapper);
     const image1 = document.createElement('div');
     image1.classList.add('image1');
+
     image1.addEventListener('click', () => {
         window.location.href = 'https://github.com/egorca6';
     });
